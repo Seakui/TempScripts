9ccbff5c6e52a84796ed5dd4c7a2bad5e8a11e9c
Seakui <117586463+Seakui@users.noreply.github.com>
2022-12-10 23:17:47 +0100

    Update MC1.lua

--- MC1.lua
+++ MC1.lua
@@ -5,7 +5,7 @@ wait(5)
 local players = game.Players:GetPlayers()
 if game.PlaceId == 10331873459 then -- The Void
 if #players < 1 or #players == 1 then
-    print("Yes")
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/TempScripts/main/MC2.lua'))()
     else
     game:GetService("TeleportService"):Teleport(8097148487)
 end
