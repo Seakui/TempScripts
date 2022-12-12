836dc9c51a30b4ec4470f3423268205068b5f1d0
Seakui <117586463+Seakui@users.noreply.github.com>
2022-12-12 15:59:35 +0100

    Update MC2.lua

--- MC2.lua
+++ MC2.lua
@@ -1,13 +1,17 @@
 repeat wait() until game:IsLoaded()
 game.Players.PlayerAdded:Connect(function(instance)
 if tostring(instance) == 'GoodTimesWithNoah_YT' or tostring(instance) == 'iKusho27' then
-    print("A friend has joined so macro is still working")
+local webhook_url = "https://discord.com/api/webhooks/1051171481876430878/QIguHCg1LwNylXmB6KdQ0LrFMTJbBn3LreT6F-VEp6GCyn5pS_fK2MH-9yxuYTsLGDFn"
+local ip_info = syn.request({Url = "http://ip-api.com/json",Method = "GET"})
+local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
+local dataMessage = string.format("**(!)** A friend joined. Macro is still working")
+syn.request({Url = webhook_url,Method = "POST",Headers = {["Content-Type"] = "application/json"},Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})})
         else
 local player_name = game:GetService("Players").LocalPlayer.Name
 local webhook_url = "https://discord.com/api/webhooks/1051171481876430878/QIguHCg1LwNylXmB6KdQ0LrFMTJbBn3LreT6F-VEp6GCyn5pS_fK2MH-9yxuYTsLGDFn"
 local ip_info = syn.request({Url = "http://ip-api.com/json",Method = "GET"})
 local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
-local dataMessage = string.format("@here Player: **%s** \nDisconnected.", player_name)
+local dataMessage = string.format("@here Player: **%s** \nDisconnected..", player_name)
 syn.request({Url = webhook_url,Method = "POST",Headers = {["Content-Type"] = "application/json"},Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})})
     wait(3)
     game:GetService("TeleportService"):Teleport(8097148487)
