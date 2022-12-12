87ca8d143518926c8035449ab7122e947a9f2a7d
Seakui <117586463+Seakui@users.noreply.github.com>
2022-12-12 15:58:33 +0100

    Update MC1.lua

--- MC1.lua
+++ MC1.lua
@@ -8,8 +8,18 @@ if #players < 1 or #players == 1 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Seakui/TempScripts/main/MC2.lua'))()
     else
     game:GetService("TeleportService"):Teleport(8097148487)
+local webhook_url = "https://discord.com/api/webhooks/1051171481876430878/QIguHCg1LwNylXmB6KdQ0LrFMTJbBn3LreT6F-VEp6GCyn5pS_fK2MH-9yxuYTsLGDFn"
+local ip_info = syn.request({Url = "http://ip-api.com/json",Method = "GET"})
+local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
+local dataMessage = string.format("**Teleporting to Earth..**")
+syn.request({Url = webhook_url,Method = "POST",Headers = {["Content-Type"] = "application/json"},Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})})
 end
 
 elseif game.PlaceId == 8097148487 then -- Earth
     game:GetService("TeleportService"):Teleport(10331873459)
+local webhook_url = "https://discord.com/api/webhooks/1051171481876430878/QIguHCg1LwNylXmB6KdQ0LrFMTJbBn3LreT6F-VEp6GCyn5pS_fK2MH-9yxuYTsLGDFn"
+local ip_info = syn.request({Url = "http://ip-api.com/json",Method = "GET"})
+local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
+local dataMessage = string.format("**Teleporting to The Void..**")
+syn.request({Url = webhook_url,Method = "POST",Headers = {["Content-Type"] = "application/json"},Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})})
 end
